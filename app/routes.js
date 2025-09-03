b53576bccbdb7242b3f223067826ca183b2c08b0
domm-hmrc <133001534+domm-hmrc@users.noreply.github.com>
2025-09-03 14:19:02 +0100

    Added v7 - simplified journey

--- app/routes.js
+++ app/routes.js
@@ -12,5 +12,6 @@ router.use('/03', require('./views/03/_routes'));
 router.use('/04', require('./views/04/_routes'));
 router.use('/05', require('./views/05/_routes'));
 router.use('/06', require('./views/06/_routes'));
+router.use('/07', require('./views/07/_routes'));
 
 module.exports = router
